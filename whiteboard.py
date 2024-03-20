11a4cda9dc146c1514039d64918e1fc46c2f034c
AaronGulman <81993286+AaronGulman@users.noreply.github.com>
2024-03-20 17:07:19 -0500

    Add canvas reset functionality and improve drawing settings(color picking)

--- whiteboard.py
+++ whiteboard.py
@@ -25,8 +25,18 @@ def locate_xy(work):
 def addLine(work):
         global current_x , current_y
         
-        canvas.create_line((current_x,current_y,work.x,work.y), width = 2 , fill = color)
+        canvas.create_line((current_x,current_y,work.x,work.y), width = 5 , fill = color)
         current_x,current_y = work.x,work.y
+
+def show_color(new_color):
+        global color
+        
+        color = new_color
+        
+def new_canvas():
+        
+        canvas.delete('all')
+        display_pallete()
 
 
 #icon
@@ -43,35 +53,35 @@ colors=Canvas(root,bg="#ffffff", width=78,height=420,bd=0)
 colors.place(x=25,y=60)
 
 eraser=PhotoImage(file="eraser.png")
-Button(root,image=eraser,bg="#f2f3f5").place(x=38,y=390)
+Button(root,image=eraser,bg="#f2f3f5" , command= new_canvas).place(x=38,y=390)
 
 def display_pallete():
         id = colors.create_rectangle((10,10,30,30),fill="#000")
-        colors.tag_bind(id,"Button-1",lambda x: show_color('#000'))
+        colors.tag_bind(id,"<Button-1>",lambda x: show_color('#000'))
         
         id = colors.create_rectangle((10,40,30,60),fill="#CFCFCF")
-        colors.tag_bind(id,"Button-1",lambda x: show_color('#CFCFCF'))
+        colors.tag_bind(id,"<Button-1>",lambda x: show_color('#CFCFCF'))
         
         id = colors.create_rectangle((10,70,30,90),fill="#7C2E1E")
-        colors.tag_bind(id,"Button-1",lambda x: show_color('#7C2E1E'))
+        colors.tag_bind(id,"<Button-1>",lambda x: show_color('#7C2E1E'))
         
         id = colors.create_rectangle((10,100,30,120),fill="#FF0000")
-        colors.tag_bind(id,"Button-1",lambda x: show_color('#FF0000'))
+        colors.tag_bind(id,"<Button-1>",lambda x: show_color('#FF0000'))
 
         id = colors.create_rectangle((10,130,30,150),fill="#FF5D00")
-        colors.tag_bind(id,"Button-1",lambda x: show_color('#FF5D00'))
+        colors.tag_bind(id,"<Button-1>",lambda x: show_color('#FF5D00'))
 
         id = colors.create_rectangle((10,160,30,180),fill="#FFF700")
-        colors.tag_bind(id,"Button-1",lambda x: show_color('#FFF700'))
+        colors.tag_bind(id,"<Button-1>",lambda x: show_color('#FFF700'))
         
         id = colors.create_rectangle((10,190,30,210),fill="#3AFF00")
-        colors.tag_bind(id,"Button-1",lambda x: show_color('#3AFF00'))
+        colors.tag_bind(id,"<Button-1>",lambda x: show_color('#3AFF00'))
         
         id = colors.create_rectangle((10,220,30,240),fill="#0051FF")
-        colors.tag_bind(id,"Button-1",lambda x: show_color('#0051FF'))
+        colors.tag_bind(id,"<Button-1>",lambda x: show_color('#0051FF'))
         
         id = colors.create_rectangle((10,250,30,270),fill="#8B00FF")
-        colors.tag_bind(id,"Button-1",lambda x: show_color('#8B00FF'))
+        colors.tag_bind(id,"<Button-1>",lambda x: show_color('#8B00FF'))
         
 display_pallete()
 

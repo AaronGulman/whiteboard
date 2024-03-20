2270c5cd81e62d042278b25be24ed150a0846e9b
AaronGulman <81993286+AaronGulman@users.noreply.github.com>
2024-03-19 21:01:09 -0500

    Add color pallete

--- whiteboard.py
+++ whiteboard.py
@@ -19,23 +19,24 @@ color_box=PhotoImage(file="color section.png")
 Label(root,image=color_box,bg="#f2f3f5").place(x=10,y=20)
 
 
-
-
-
-
-color=Canvas(root,bg="#ffffff", width=78,height=420,bd=0)
-color.place(x=25,y=60)
+colors=Canvas(root,bg="#ffffff", width=78,height=420,bd=0)
+colors.place(x=25,y=60)
 
 eraser=PhotoImage(file="eraser.png")
 Button(root,image=eraser,bg="#f2f3f5").place(x=38,y=390)
 
-
+def display_pallete():
+        id = colors.create_rectangle((10,10,30,30),fill="#000")
+        colors.tag_bind(id,"Button-1",lambda x: show_color('#000'))
+        
+display_pallete()
 
 canvas = Canvas(root,width=930,height=500,background="#fff",cursor="hand2")
 canvas.place(x=100,y=10)
 
 
-
+canvas.bind('<Button-1>')
+canvas.bind('<B1-Motion>')
 
 
 

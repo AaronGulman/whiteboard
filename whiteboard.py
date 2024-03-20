7b5fed3e7eb02045de72c4bcd53d2a292845ca94
AaronGulman <81993286+AaronGulman@users.noreply.github.com>
2024-03-19 20:42:55 -0500

    Add icon and color section to Whiteboard GUI

--- whiteboard.py
+++ whiteboard.py
@@ -8,4 +8,17 @@ root.title("Whiteboard")
 root.geometry("1050x570+150+50")
 root.configure(bg="#f2f3f5")
 root.resizable(False,False)
+
+#icon
+image_icon = PhotoImage(file="logo.png")
+root.iconphoto(False,image_icon)
+
+
+
+color_box=PhotoImage(file="color section.png")
+Label(root,image=color_box,bg="#f2f3f5").place(x=10,y=20)
+
+
+
 root.mainloop()
+

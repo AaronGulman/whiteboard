1b9fbf6fe6785aff70dcfe33618c0371607699ac
AaronGulman <81993286+AaronGulman@users.noreply.github.com>
2024-03-19 19:59:51 -0500

    Add initial setup for Whiteboard app

---
+++ whiteboard.py
@@ -0,0 +1,11 @@
+from tkinter import *
+from tkinter.colorchooser import askcolor
+from tkinter import ttk
+import tkinter as Ttk
+
+root = Tk()
+root.title("Whiteboard")
+root.geometry("1050x570+150+50")
+root.configure(bg="#f2f3f5")
+root.resizable(False,False)
+root.mainloop()
